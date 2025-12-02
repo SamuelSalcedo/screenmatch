14efedcbb56d72c7503992abbf51256c05e84299
SamuelSalcedo <salcedolucerosamuel2@gmail.com>
2025-12-02 00:28:36 -0600

    Iniciair proyecto

---
+++ src/main/java/com/alura/screematch/ScreematchApplication.java
@@ -0,0 +1,19 @@
+package com.alura.screematch;
+
+import com.sun.tools.jconsole.JConsoleContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("HOLA WE");
+    }
+}
