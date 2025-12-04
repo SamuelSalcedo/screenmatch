e629be97c35cd5e9b2921cd5c10bea0fe5cad03d
SamuelSalcedo <salcedolucerosamuel2@gmail.com>
2025-12-04 01:22:40 -0600

    prueba de API y config

--- src/main/java/com/alura/screematch/ScreematchApplication.java
+++ src/main/java/com/alura/screematch/ScreematchApplication.java
@@ -1,9 +1,11 @@
 package com.alura.screematch;
 
-import com.sun.tools.jconsole.JConsoleContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.alura.screematch.service.*;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreematchApplication implements CommandLineRunner {
@@ -14,6 +16,19 @@ public class ScreematchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         System.out.println("HOLA WE");
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingresa el nombre de la serie que buscas: \n");
+        //var busqueda = leer.nextLine();
+
+        //String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=d31d9f71";
+       // String direccion = "https://www.omdbapi.com/?t=breaking%20bad&apikey=d31d9f71";
+
+        var consumible = new consumoAPI();
+
+        var json = consumible.obtenerDatos(direccion);
+        System.out.println(json);
     }
 }
---
+++ src/main/java/com/alura/screematch/model/datoSerie.java
@@ -0,0 +1,5 @@
+package com.alura.screematch.model;
+
+public class datoSerie {
+
+}
